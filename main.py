48c9f39739c23a112a015ad68b583a5c82c6c004
Gullwinkle <gullwinkle@gmail.com>
2025-01-26 14:59:13 +0500

    Update main.py
    added menu, file save and load

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import filedialog
 
 def add_task():
     task = task_entry.get()
@@ -31,15 +32,80 @@ def finish_task():
         finished_listbox.insert(tk.END, started_listbox.get(selected_task))
         started_listbox.delete(selected_task)
 
+def save_tasks():
+    global file_name
+    if file_name:
+        task_list = task_listbox.get(0, tk.END)
+        started_list = started_listbox.get(0, tk.END)
+        finished_list = finished_listbox.get(0, tk.END)
+        with open(file_name, 'w', encoding='utf-8') as file:
+            file.write(','.join(task_list))
+            file.write('\n')
+            file.write(','.join(started_list))
+            file.write('\n')
+            file.write(','.join(finished_list))
+    else:
+        save_tasks_as()
+def save_tasks_as():
+    global file_name
+    file_name = filedialog.asksaveasfilename(defaultextension='.txt', filetypes=[('Text files', '*.txt'), ('All files', '*.*')])
+    root.title(f'Task list - {file_name.split("/")[-1]}')
+    save_tasks()
+
+def load_tasks():
+    global file_name
+    file_name = filedialog.askopenfilename()
+    root.title(f'Task list - {file_name.split("/")[-1]}')
+    task_listbox.delete(0, tk.END)
+    started_listbox.delete(0, tk.END)
+    finished_listbox.delete(0, tk.END)
+    try:
+        with open(file_name, 'r') as file:
+            tasks = file.read().split('\n')
+            i_task = 0
+            i_started = 0
+            i_finished = 0
+            for task in tasks[0].split(','):
+                task_listbox.insert(i_task, task)
+                i_task += 1
+            for task in tasks[1].split(','):
+                started_listbox.insert(i_started, task)
+                i_started += 1
+            for task in tasks[2].split(','):
+                finished_listbox.insert(i_finished, task)
+                i_finished += 1
+    except FileNotFoundError:
+        pass
+
+def new_file():
+    global file_name
+    file_name = None
+    root.title('Task list')
+    task_listbox.delete(0, tk.END)
+    started_listbox.delete(0, tk.END)
+    finished_listbox.delete(0, tk.END)
 
 root = tk.Tk()
+file_name = None
 root.title('Task list')
 root.configure(background='antiquewhite3')
+
+main_menu = tk.Menu(root)
+file_menu = tk.Menu(main_menu, tearoff=0)
+file_menu.add_command(label='Новый', command=new_file)
+file_menu.add_command(label='Открыть', command=load_tasks)
+file_menu.add_command(label='Сохранить', command=save_tasks)
+file_menu.add_command(label='Сохранить как...', command=save_tasks_as)
+file_menu.add_separator()
+file_menu.add_command(label='Выход', command=root.quit)
+main_menu.add_cascade(label='Файл', menu=file_menu)
+root.config(menu=main_menu)
 
 text1 = tk.Label(root, text='Введите вашу задачу:', bg='antiquewhite3')
 text1.grid(column=0, row=0, pady=5)
 
 task_entry = tk.Entry(root, width=30, bg='antiquewhite4')
+task_entry.bind('<Return>', lambda event: add_task())
 task_entry.grid(column=0, row=1, pady=10)
 
 add_task_button = tk.Button(root, text='Добавить задачу', command=add_task, width=40)
